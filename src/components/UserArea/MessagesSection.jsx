2e7b3e3e91c8227875cd419ef86ad252ef333ebb
jacrifla <jaquelini_cris@hotmail.com>
2024-12-17 20:41:05 -0300

    style

--- src/components/UserArea/MessagesSection.jsx
+++ src/components/UserArea/MessagesSection.jsx
@@ -10,66 +10,65 @@ function MessagesSection({ tokens, onApproval, isLoading }) {
   };
 
   useEffect(() => {
-    // Resetar o estado de 'selectedToken' se o token for processado.
     if (isTokenProcessed) {
-      setSelectedToken("");  // Limpa a seleção após processamento
+      setSelectedToken(""); // Limpa a seleção após processamento
     }
   }, [isTokenProcessed]);
-
-  if (isLoading) {
-    return <p className="text-gray-500">Carregando tokens...</p>;
-  }
 
   return (
     <div className="flex-1 ml-4">
       <h3 className="text-2xl font-semibold mb-4">Mensagens</h3>
-      <div className="bg-gray-50 p-4 border border-gray-200 rounded shadow-sm">
-        <label className="block mb-2">
-          Selecione um token de compartilhamento:
-        </label>
+      {isLoading ? (
+        <div className="min-h-[200px] flex items-center justify-center">
+          <p className="text-gray-500">Carregando tokens...</p>
+        </div>
+      ) : (
+        <div className="bg-gray-50 p-4 border border-gray-200 rounded shadow-sm">
+          <label className="block mb-2">Selecione um token de compartilhamento:</label>
 
-        {/* Se não houver tokens, exibe mensagem */}
-        {tokens.length === 0 ? (
-          <p className="text-gray-500">Não há tokens pendentes no momento.</p>
-        ) : (
-          <>
-            {!isTokenProcessed && (
-              <select
-                className="w-full p-2 border border-gray-300 rounded mb-4"
-                onChange={(e) => setSelectedToken(e.target.value)}
-                value={selectedToken}
-                disabled={isTokenProcessed} // Desabilita o select após o processamento
+          {/* Se não houver tokens, exibe mensagem */}
+          {tokens.length === 0 ? (
+            <p className="text-gray-500">Não há tokens pendentes no momento.</p>
+          ) : (
+            <>
+              {!isTokenProcessed && (
+                <select
+                  className="w-full p-2 border border-gray-300 rounded mb-4"
+                  onChange={(e) => setSelectedToken(e.target.value)}
+                  value={selectedToken}
+                  disabled={isTokenProcessed}
+                >
+                  <option value="">Escolha um token</option>
+                  {tokens.map((token) => (
+                    <option key={token.id} value={token.token}>
+                      {token.token} - Status: {token.status}
+                    </option>
+                  ))}
+                </select>
+              )}
+            </>
+          )}
+
+          {selectedToken && !isTokenProcessed && (
+            <div className="flex justify-between">
+              <button
+                className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-700"
+                onClick={() => handleApproval(selectedToken, true)}
+                disabled={isTokenProcessed}
               >
-                <option value="">Escolha um token</option>
-                {tokens.map((token) => (
-                  <option key={token.id} value={token.token}>
-                    {token.token} - Status: {token.status}
-                  </option>
-                ))}
-              </select>
-            )}
-          </>
-        )}
-
-        {selectedToken && !isTokenProcessed && (
-          <div className="flex justify-between">
-            <button
-              className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-700"
-              onClick={() => handleApproval(selectedToken, true)}
-              disabled={isTokenProcessed}
-            >
-              Aprovar
-            </button>
-            <button
-              className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-700"
-              onClick={() => handleApproval(selectedToken, false)}
-              disabled={isTokenProcessed}
-            >
-              Rejeitar
-            </button>
-          </div>
-        )}
-      </div>
+                Aprovar
+              </button>
+              <button
+                className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-700"
+                onClick={() => handleApproval(selectedToken, false)}
+                disabled={isTokenProcessed}
+              >
+                Rejeitar
+              </button>
+            </div>
+          )}
+        </div>
+      )}
     </div>
   );
 }
